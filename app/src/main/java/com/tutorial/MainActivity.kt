c0460f56d83d5918974e2fbe392ec0381f1bc000
Budhi Octaviansyah <budioct@gmail.com>
2024-02-08 19:09:14 +0700

    docs: App Component - activity android

--- app/src/main/java/com/tutorial/MainActivity.kt
+++ app/src/main/java/com/tutorial/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * ini adalah class code untuk Manifest
+     */
+
+    // (callback function) ketika activity di buat method ini yang akan di eksekusi
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState) // onCreate // Salah satu method yang dipanggil ketika Activity dibuat
+        setContentView(R.layout.activity_main) // setContentView() // menampilkan view UI layout
     }
 }
