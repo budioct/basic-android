3a0d1b5a6970949d769a21e7704ca6db08c54bd9
Budhi Octaviansyah <budioct@gmail.com>
2024-02-09 09:26:41 +0700

    docs: App Component - activity android update

--- app/src/main/java/com/tutorial/MainActivity.kt
+++ app/src/main/java/com/tutorial/MainActivity.kt
@@ -7,8 +7,11 @@ class MainActivity : AppCompatActivity() {
 
     /**
      * ini adalah class code untuk Manifest
+     * class MainActivity adalah class yang memproses file Manifest.xml (informasi android yang di buat)
+     * class AppCompatActivity adalah turunan dari class Activity yang memungkingkan menggunakan fitur baru Android di versi Android lama (Di Rekomendasikan)
      */
 
+    // activity callback, function onCreate() dipanggil ketika activity dibuat
     // (callback function) ketika activity di buat method ini yang akan di eksekusi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState) // onCreate // Salah satu method yang dipanggil ketika Activity dibuat
